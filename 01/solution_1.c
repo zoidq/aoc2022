af67a93c2a58be76ecab5335418011ebeaa431d5
Nick <20363253+zoidq@users.noreply.github.com>
2022-12-01 21:02:05 -0400

    Fixed formatting

--- 01/solution_1.c
+++ 01/solution_1.c
@@ -1,32 +1,37 @@
-#include <stdio.h>
+ﬂ#include <stdio.h>
 #include <stdlib.h>
 
-int main() 
+int main()
 {
-		FILE *fp = fopen("input.txt", "r");
-		if(fp == NULL) {
-				perror("Unable to open input.txt");
-				exit(1);
+	FILE *fp = fopen("input.txt", "r");
+	if (fp == NULL)
+	{
+		perror("Unable to open input.txt");
+		exit(1);
+	}
+
+	int maxCal = 0;
+	int curCal = 0;
+	char *line = NULL;
+	size_t linecap = 0;
+	while ((getline(&line, &linecap, fp)) > 0)
+	{
+		if (line[0] == '\n')
+		{
+			curCal = 0;
+			continue;
 		}
 
-		int maxCal = 0;
-		int curCal = 0;
-		char *line = NULL;
-		size_t linecap = 0;
-		while((getline(&line, &linecap, fp)) > 0) {
-				if(line[0] == '\n') {
-						curCal = 0;
-						continue;
-				}
-				
-				curCal += atoi(line);
-				if(curCal > maxCal) { 
-						maxCal = curCal;
-				}
+		curCal += atoi(line);
+		if (curCal > maxCal)
+		{
+			maxCal = curCal;
 		}
+	}
 
-		printf("max calories: %d\n", maxCal);;
+	printf("max calories: %d\n", maxCal);
+	;
 
-		fclose(fp);
-		return 0;
+	fclose(fp);
+	return 0;
 }
--- 01/solution_2.c
+++ 01/solution_2.c
@@ -3,42 +3,50 @@
 
 void rank(const int total, int *top)
 {
-		if(total > top[0]) {
-				top[2] = top[1];
-				top[1] = top[0];
-				top[0] = total;
-		} else if(total > top[1]) {
-				top[2] = top[1];
-				top[1] = total;
-		} else if(total > top[2]) {
-				top[2] = total;
-		}
+	if (total > top[0])
+	{
+		top[2] = top[1];
+		top[1] = top[0];
+		top[0] = total;
+	}
+	else if (total > top[1])
+	{
+		top[2] = top[1];
+		top[1] = total;
+	}
+	else if (total > top[2])
+	{
+		top[2] = total;
+	}
 }
 
-int main() 
+int main()
 {
-		FILE *fp = fopen("input.txt", "r");
-		if(fp == NULL) {
-				perror("Unable to open input.txt");
-				exit(1);
+	FILE *fp = fopen("input.txt", "r");
+	if (fp == NULL)
+	{
+		perror("Unable to open input.txt");
+		exit(1);
+	}
+
+	int top[] = {0, 0, 0};
+	int curCal = 0;
+	char *line = NULL;
+	size_t linecap = 0;
+
+	while ((getline(&line, &linecap, fp)) > 0)
+	{
+		if (line[0] == '\n')
+		{
+			rank(curCal, top);
+			curCal = 0;
+			continue;
 		}
+		curCal += atoi(line);
+	}
 
-		int top[] = {0, 0, 0};
-		int curCal = 0;
-		char *line = NULL;
-		size_t linecap = 0;
-
-		while((getline(&line, &linecap, fp)) > 0) {
-				if(line[0] == '\n') {
-						rank(curCal, top);
-						curCal = 0;
-						continue;
-				}
-				curCal += atoi(line);
-		}
-
-		printf("top three: %d %d %d\n", top[0], top[1], top[2]);
-		printf("sum calories: %d\n", top[0] + top[1] + top[2]);
-		fclose(fp);
-		return 0;
+	printf("top three: %d %d %d\n", top[0], top[1], top[2]);
+	printf("sum calories: %d\n", top[0] + top[1] + top[2]);
+	fclose(fp);
+	return 0;
 }
