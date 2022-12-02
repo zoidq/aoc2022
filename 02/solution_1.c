383abd050f28bd7829d503d525e20577bc1ccf47
Nick <20363253+zoidq@users.noreply.github.com>
2022-12-02 18:16:09 -0400

    Day 2 solutions

---
+++ 02/solution_1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MOVE_ASCII_DIFF 23
+
+void result(const int opponentMove, const int myMove, int *score)
+{
+    if ((opponentMove == 1 && myMove == 2) || (opponentMove == 2 && myMove == 3) || (opponentMove == 3 && myMove == 1))
+        *score += 6;
+
+    else if ((opponentMove == myMove))
+        *score += 3;
+
+    *score += myMove;
+}
+
+int main()
+{
+    FILE *fp = fopen("input.txt", "r");
+    if (fp == NULL)
+    {
+        perror("Unable to open input.txt");
+        exit(1);
+    }
+
+    char *line = NULL;
+    size_t linecap = 0;
+    char opponentMove, myMove;
+    int score = 0;
+    while ((getline(&line, &linecap, fp)) > 0)
+    {
+        opponentMove = line[0] - 'A' + 1;
+        myMove = line[2] - 'A' - MOVE_ASCII_DIFF + 1;
+        result(opponentMove, myMove, &score);
+    }
+
+    printf("%d\n", score);
+}
---
+++ 02/solution_2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void result(const char opponentMove, const char outcome, int *score)
+{
+    switch (outcome)
+    {
+    case 'X':
+        switch (opponentMove)
+        {
+        case 'A':
+            *score += 3;
+            break;
+        case 'B':
+            *score += 1;
+            break;
+        case 'C':
+            *score += 2;
+            break;
+        }
+        break;
+
+    case 'Y':
+        *score += 3 + (opponentMove - 'A') + 1;
+        break;
+
+    case 'Z':
+        switch (opponentMove)
+        {
+        case 'A':
+            *score += 2;
+            break;
+        case 'B':
+            *score += 3;
+            break;
+        case 'C':
+            *score += 1;
+            break;
+        }
+        *score += 6;
+    }
+}
+
+int main()
+{
+    FILE *fp = fopen("input.txt", "r");
+    if (fp == NULL)
+    {
+        perror("Unable to open input.txt");
+        exit(1);
+    }
+
+    char *line = NULL;
+    size_t linecap = 0;
+    int score = 0;
+    while ((getline(&line, &linecap, fp)) > 0)
+    {
+        result(line[0], line[2], &score);
+    }
+
+    printf("%d\n", score);
+}
