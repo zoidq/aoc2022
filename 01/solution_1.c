4fa4fd26ecdc05ae950ea701c5c5cd1df868cd57
Nick <20363253+zoidq@users.noreply.github.com>
2022-12-01 20:49:41 -0400

    Initial commit

---
+++ 01/solution_1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+		FILE *fp = fopen("input.txt", "r");
+		if(fp == NULL) {
+				perror("Unable to open input.txt");
+				exit(1);
+		}
+
+		int maxCal = 0;
+		int curCal = 0;
+		char *line = NULL;
+		size_t linecap = 0;
+		while((getline(&line, &linecap, fp)) > 0) {
+				if(line[0] == '\n') {
+						curCal = 0;
+						continue;
+				}
+				
+				curCal += atoi(line);
+				if(curCal > maxCal) { 
+						maxCal = curCal;
+				}
+		}
+
+		printf("max calories: %d\n", maxCal);;
+
+		fclose(fp);
+		return 0;
+}
---
+++ 01/solution_2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void rank(const int total, int *top)
+{
+		if(total > top[0]) {
+				top[2] = top[1];
+				top[1] = top[0];
+				top[0] = total;
+		} else if(total > top[1]) {
+				top[2] = top[1];
+				top[1] = total;
+		} else if(total > top[2]) {
+				top[2] = total;
+		}
+}
+
+int main() 
+{
+		FILE *fp = fopen("input.txt", "r");
+		if(fp == NULL) {
+				perror("Unable to open input.txt");
+				exit(1);
+		}
+
+		int top[] = {0, 0, 0};
+		int curCal = 0;
+		char *line = NULL;
+		size_t linecap = 0;
+
+		while((getline(&line, &linecap, fp)) > 0) {
+				if(line[0] == '\n') {
+						rank(curCal, top);
+						curCal = 0;
+						continue;
+				}
+				curCal += atoi(line);
+		}
+
+		printf("top three: %d %d %d\n", top[0], top[1], top[2]);
+		printf("sum calories: %d\n", top[0] + top[1] + top[2]);
+
+		/*
+		 * incorrect:
+		 * top three: 72017 71144 68735
+		 * sum calories: 211896
+		 *
+		 *
+		 * top three: 72017 71144 69359
+		 * sum caloriess: 212520
+		 */
+
+		fclose(fp);
+		return 0;
+}
